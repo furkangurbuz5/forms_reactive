77a04706d9def2d708def2410cfbc58e8836f662
furkang-risa <Furkan@risa-it.nl>
2026-03-11 13:38:12 +0100

    Bind most fields to FG

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import {
+  AbstractControl,
   FormControl,
   FormGroup,
   FormsModule,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+
+function arePasswordsEqual(control: AbstractControl) {
+  if (control) {
+    return null;
+  } else return { passwordNotEqual: true };
+}
+
+type Roles = 'student' | 'teacher' | 'employee' | 'founder' | 'other';
 
 @Component({
   selector: 'app-signup',
@@ -22,10 +31,46 @@ export class SignupComponent {
     password: new FormControl('', {
       validators: [Validators.required, Validators.minLength(6)],
     }),
+    confirmPassword: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.minLength(6),
+        arePasswordsEqual,
+      ],
+    }),
+    firstName: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    lastName: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    street: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    streetNumber: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    postalCode: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    city: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    role: new FormControl<Roles>('student', {
+      validators: [Validators.required],
+    }),
+    agreeToTermsAndConditions: new FormControl(false, {
+      validators: [Validators.required],
+    }),
   });
 
-  onSubmit() {}
+  onSubmit() {
+    console.warn('Form Submitted!');
+    console.log(this.signupForm.controls['agreeToTermsAndConditions'].value);
+    console.log(this.signupForm.controls['role'].value);
+  }
   onReset() {
+    console.warn('Form Reset!');
     this.signupForm.reset();
   }
 }
